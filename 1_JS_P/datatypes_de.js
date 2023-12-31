d34331c9c9af70b0541bf5275040c2be6a711292
SAGNIK SAHA <126655769+sagnik81599@users.noreply.github.com>
2023-12-31 05:41:15 +0000

    variables

---
+++ 1_JS_P/datatypes_de.js
@@ -0,0 +1,25 @@
+
+//alert("") // It use only brower 
+
+
+let name = "Sagnik"
+let roll = 20
+let nickname = null
+let boolean = false
+let state;
+
+
+
+// number => 2 to power 53
+// bigint
+// String => ""
+// boolean => True or False
+// Null => Standalong value
+// undefined =>
+// symbol => unique
+
+// object 
+
+console.log(typeof(roll))
+console.log(typeof null) // object
+
---
+++ 1_JS_P/variable_de.js
@@ -0,0 +1,21 @@
+const roll = 2563
+let student_Email = "Sagnik@gmail.com"
+let student_pass = "3652487"
+let city = "Kolkata"
+let faculty;
+//roll = 2  // not changing value 
+
+console.log(roll);
+
+student_Email = "Sagniksaha@google.com"
+student_pass = "783684"
+city ="Chennai"
+
+/* Do not use var 
+   because of issue in block scope and functional scope */
+
+console.table([student_Email,student_pass,city,faculty])
+
+
+
+
