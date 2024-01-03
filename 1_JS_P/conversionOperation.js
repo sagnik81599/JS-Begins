b3a2e6e14b50e0bd7502491e2591c0667cef8051
SAGNIK SAHA <126655769+sagnik81599@users.noreply.github.com>
2024-01-03 13:09:33 +0000

    conversionOperation

---
+++ 1_JS_P/conversionOperation.js
@@ -0,0 +1,19 @@
+// *************** Operation ******************
+
+let value = 5
+let negavalue = -value
+console.log(negavalue);
+//Ans:- nagative value -5 
+
+// console.log(5+2)
+// console.log(5-2)
+// console.log(5/2)
+// console.log(5*2)
+// console.log(3**3)
+
+console.log(1+2+" 45")
+console.log("2"+3+7);
+
+
+
+
