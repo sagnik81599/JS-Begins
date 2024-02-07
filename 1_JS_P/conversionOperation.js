2b24cca7ba4f1790daab099ce87c039e765be9b3
SAGNIK SAHA <126655769+sagnik81599@users.noreply.github.com>
2024-02-07 19:10:08 +0000

    containeer commit done

--- 1_JS_P/conversionOperation.js
+++ 1_JS_P/conversionOperation.js
@@ -12,7 +12,24 @@ console.log(negavalue);
 // console.log(3**3)
 
 console.log(1+2+" 45")
-console.log("2"+3+7);
+console.log("2"+3+7)
+// ans:- 3 45
+//ans:- 237
+
+console.log(+true)
+console.log(+"")
+
+//ans: 1,0
+
+
+let x=2
+let y=3
+
+y=++x+y+x++
+console.log(y)
+//ans:- 
+t= ++x+y-y+x+y++
+console.log(t)
 
 
 
