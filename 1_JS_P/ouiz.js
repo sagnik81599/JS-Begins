872e341c255185af425fb1f98827e0d4a8d4f626
SAGNIK SAHA <126655769+sagnik81599@users.noreply.github.com>
2024-07-21 17:37:29 +0000

    changes

--- 1_JS_P/ouiz.js
+++ 1_JS_P/ouiz.js
@@ -5,7 +5,7 @@ console.log(typeof b)
 // ANS:- Number because '+' make type as number  
 //there's something in JavaScript called type casting that give ability to,
 //machine to convert number as string and deal with it as number
-
+//---------------------------------------------------------------------------------
 
 const ab = "Sagnik"
 console.log(`Hello ${ab}`)
@@ -23,6 +23,7 @@ for(let i=0;i<num.length;i++){
 
 //Arrays are 0 indexed. The continue skips the console log. When i = 2 (value 3), 
 //it doesn't print. Final print: 1,2,4
+//----------------------------------------------------------------------------------------
 
 let n = [2,3,4,5,6]
 
@@ -34,5 +35,6 @@ for(let j=0;j<n.length;j++){
 }
 
 //Ans:- 2,3 beecause index no.2 to upto index wiil be stop
+//---------------------------------------------------------------------------
 
 
