086e96e3a74bba8c02dd36eef1b962bb267e96e2
SAGNIK SAHA <126655769+sagnik81599@users.noreply.github.com>
2026-07-09 09:47:18 +0000

    Updated JavaScript programs

---
+++ sync&async/p1.js
@@ -0,0 +1,4 @@
+// Synchronous code executes one line at a time. The next line waits until the current line finishes.
+//Asynchronous code does not block the rest of the program. Long-running tasks are started, and JavaScript continues executing other code while waiting.
+
+//javascript is a sync but later it was async by callback functions
