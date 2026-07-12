416aaec82144171232460f03666ea747e72b288d
SAGNIK SAHA <126655769+sagnik81599@users.noreply.github.com>
2026-07-12 16:08:39 +0000

    Updated JavaScript programs

--- DOM Manipulation/select_items.js
+++ DOM Manipulation/select_items.js
@@ -19,6 +19,37 @@ console.log(p);
 //next change text 
 //<h1 id="title">Hello</h1>
 
-let t = document.getElementById("title");
+let title = document.getElementById("title");
 title.innerText="welcome";
+
+
+
+{/* <h1 id ="title">hello</h1> */}
+
+
+let title = document.getElementById("title");
+title.style.color = "red";
+title.style.backgroundColor = "yellow";
+title.style.fontSize = "40px";
+
+
+<button id="sumbit">sumbit</button>
+
+let btn = document.getElementById("sumbit");
+btn.addEventListener("click", function(){
+    documnet.getElementById("msg").innertext="button clicked";
+})
+
+
+{/* <div id="container"></div> */}
+
+let p = document.createElement("p");
+
+p.innerText = "Hello DOM";
+
+document.getElementById("container").appendChild(p);
+
+
+
+
 
