7ecf2ab266004b5c0090efa8fa838cc7ad3f9673
SAGNIK SAHA <126655769+sagnik81599@users.noreply.github.com>
2026-07-13 13:33:53 +0000

    Updated JavaScript programs

---
+++ react/deatils.js
@@ -0,0 +1,19 @@
+// react :- js library which carte web application
+// - single page application
+// -virtual DOM
+
+//npm - node package manager
+//npx - node package executer
+//npx create-react-app projectname
+//npx create-react-app .// for using the same folder as project
+
+//npm create vite@latest .
+
+// 3 pillers of react
+
+//-component
+//-> class     //support this
+//-> function //not support dynamic this
+
+//-  props
+// - state
