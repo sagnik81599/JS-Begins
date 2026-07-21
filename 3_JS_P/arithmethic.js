8f80b2748dbdb4b39ed1cfd1d878548bd4131120
SAGNIK SAHA <126655769+sagnik81599@users.noreply.github.com>
2026-07-21 16:22:20 +0000

    updated

---
+++ 3_JS_P/arithmethic.js
@@ -0,0 +1,44 @@
+
+const a = 18;
+const b = 24;
+
+// Addition of two values
+function add() {
+    return a+b;
+}
+console.log(add())
+
+// Subtract small value from larger one
+function subtract() {
+    return b-a;
+}
+console.log(subtract())
+
+function multiply() {
+    return a*b;
+}
+console.log(multiply())
+
+// Divide larger value by small
+function divide() {
+    return b/a;
+}
+console.log(divide())
+
+// Increase value of a by 1
+function increment() {
+    return a+ 1;
+}
+console.log(increment())
+
+// Decrease value of b by 1
+function decrement() {
+    return b-1;
+}
+console.log(decrement())
+
+// Divide larger value by small to find the reminder
+function reminder() {
+    return b%a;
+}
+console.log(reminder())
